2ab407461d768d7f9b3930794546f96684526109
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-06-25 17:34:34 -0400

    Rendered the undo_seed and seed_all methods for our channels. Created test channels

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,3 +1,4 @@
+from app.seeds.channel import seed_channels, undo_seed_channels
 from app.seeds.servers import seed_servers, undo_seed_servers
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
@@ -11,13 +12,15 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     seed_users()
-    seed_servers()
     # Add other seed functions here
+    seed_servers()
+    seed_channels()
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
     undo_users()
-    undo_seed_servers
     # Add other undo functions here
+    undo_seed_servers()
+    undo_seed_channels()
