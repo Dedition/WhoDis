bea87db567d7e61d9d1671d576e6c3da92c536e5
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-06-25 17:09:29 -0400

    Rendered the undo_seed and seed_all methods for our servers. Created test seeder

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,3 +1,4 @@
+from app.seeds.servers import seed_servers, undo_seed_servers
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 
@@ -10,6 +11,7 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     seed_users()
+    seed_servers()
     # Add other seed functions here
 
 
@@ -17,4 +19,5 @@ def seed():
 @seed_commands.command('undo')
 def undo():
     undo_users()
+    undo_seed_servers
     # Add other undo functions here
