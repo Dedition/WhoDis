196c196cabe57159dcb0cb1536dc7157c3fec4bb
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-06-26 02:09:43 -0400

    Added first route for serveer

--- app/api/server_routes.py
+++ app/api/server_routes.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, jsonify
+from ..models.db import Server, db
 
 server_routes = Blueprint('server', __name__, url_prefix="/servers")
 
 
 @server_routes.route('/', methods=["GET"])
 def all_servers():
+    servers = Server.query.all()
+    return {'servers': [server.to_dict() for server in servers]}
