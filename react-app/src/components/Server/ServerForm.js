062171f0246740e653efbec18d12f83aa535916f
Sona Jasani <jasanisona@gmail.com>
2022-06-27 17:09:49 -0400

    created serverForm .js and .css

---
+++ react-app/src/components/Server/ServerForm.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Redirect, useHistory, NavLink } from 'react-router-dom';
+
+import "./serverForm.css"
