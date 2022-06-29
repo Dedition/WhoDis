55d350f7942f17605837186d63f91cdccccbbff1
Weiqi Mei <weiqimei@ucsb.edu>
2022-06-28 22:50:36 -0700

    added animation

--- react-app/src/components/ChannelForm/ChannelForm.js
+++ react-app/src/components/ChannelForm/ChannelForm.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Redirect, useHistory, NavLink } from 'react-router-dom';
 
 import { addSingleChannel } from '../../store/channels'
-
+import "./ChannelForm.css"
 
 
 const ChannelForm = () => {
@@ -48,6 +48,47 @@ const ChannelForm = () => {
         </button>
       </form>
     </div>
+      {/* animation */}
+      {/* <div class="left">
+        <div class="cat">
+          <div class="ears1"></div>
+          <div class="head1">
+            <div class="eyes1"></div>
+            <div class="nose1"></div>
+          </div>
+          <div class="body1">
+            <div class="left-paw1"></div>
+            <div class="right-paw1"></div>
+          </div>
+          <div class="tail1"></div>
+          <div class="PRlaptop">
+            <div class="PRscreen"></div>
+            <div class="PRkeyboard"></div>
+          </div>
+        </div>
+      </div>
+      <div class="right">
+        <div class="dog">
+          <div class="ears2"></div>
+          <div class="head2">
+            <div class="eyes2"></div>
+            <div class="nose2"></div>
+          </div>
+          <div class="body2">
+            <div class="left-paw2"></div>
+            <div class="right-paw2"></div>
+          </div>
+          <div class="tail2"></div>
+          <div class="ORlaptop">
+            <div class="ORscreen"></div>
+            <div class="ORkeyboard"></div>
+          </div>
+        </div>
+      </div> */}
+
+
+
+      
     </div>
   )
 
--- react-app/src/components/ServerForm/ServerForm.js
+++ react-app/src/components/ServerForm/ServerForm.js
@@ -53,66 +53,104 @@ const ServerForm = () => {
                 <form onSubmit={submitForm}>
                     <div className='create-text'>Create New Server!</div>
                     <div>
-                    <label htmlFor='name'>Name</label>
-                    <input
-                        name="name"
-                        type="text"
-                        placeholder="Name"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        required
-                    />
+                        <label htmlFor='name'>Name</label>
+                        <input
+                            name="name"
+                            type="text"
+                            placeholder="Name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            required
+                        />
                     </div>
                     <div>
                         <label htmlFor='banner_url'>Banner Img</label>
-                    <input
-                        name="banner_url"
-                        type="text"
-                        placeholder="Banner Img"
-                        value={banner_url}
-                        onChange={(e) => setBannerUrl(e.target.value)}
-                        required
-                    />
+                        <input
+                            name="banner_url"
+                            type="text"
+                            placeholder="Banner Img"
+                            value={banner_url}
+                            onChange={(e) => setBannerUrl(e.target.value)}
+                            required
+                        />
                     </div>
                     <div>
                         <label htmlFor="server_icon_url">Server Icon Img</label>
-                    <input
-                        name="server_icon_url"
-                        type="text"
-                        placeholder="Server Icon Img"
-                        value={server_icon_url}
-                        onChange={(e) => setServerIconUrl(e.target.value)}
-                        required
-                    />
+                        <input
+                            name="server_icon_url"
+                            type="text"
+                            placeholder="Server Icon Img"
+                            value={server_icon_url}
+                            onChange={(e) => setServerIconUrl(e.target.value)}
+                            required
+                        />
                     </div>
                     <div className='public-private'>
-                    <label htmlFor="public">
-                        Public
-                        <input
-                            type="checkbox"
-                            value="no"
-                            name="public"
-                            checked={notPrivate === 'no'}
-                            onChange={(e) => setNotPrivate('no')}
-                        />
-                    </label>
-                    <label htmlFor="public">
-                        Private
-                        <input
-                            type="checkbox"
-                            value="yes"
-                            name="public"
-                            checked={notPrivate === 'yes'}
-                            onChange={(e) => setNotPrivate('yes')}
-                        />
-                    </label>
+                        <label htmlFor="public">
+                            Public
+                            <input
+                                type="checkbox"
+                                value="no"
+                                name="public"
+                                checked={notPrivate === 'no'}
+                                onChange={(e) => setNotPrivate('no')}
+                            />
+                        </label>
+                        <label htmlFor="public">
+                            Private
+                            <input
+                                type="checkbox"
+                                value="yes"
+                                name="public"
+                                checked={notPrivate === 'yes'}
+                                onChange={(e) => setNotPrivate('yes')}
+                            />
+                        </label>
                     </div>
                     <div>
-                    <button type="submit">
-                        Submit
-                    </button>
+                        <button type="submit">
+                            Submit
+                        </button>
                     </div>
                 </form>
+            </div>
+
+            {/* animation */}
+            <div class="left">
+                <div class="cat">
+                    <div class="ears1"></div>
+                    <div class="head1">
+                        <div class="eyes1"></div>
+                        <div class="nose1"></div>
+                    </div>
+                    <div class="body1">
+                        <div class="left-paw1"></div>
+                        <div class="right-paw1"></div>
+                    </div>
+                    <div class="tail1"></div>
+                    <div class="PRlaptop">
+                        <div class="PRscreen"></div>
+                        <div class="PRkeyboard"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="right">
+                <div class="dog">
+                    <div class="ears2"></div>
+                    <div class="head2">
+                        <div class="eyes2"></div>
+                        <div class="nose2"></div>
+                    </div>
+                    <div class="body2">
+                        <div class="left-paw2"></div>
+                        <div class="right-paw2"></div>
+                    </div>
+                    <div class="tail2"></div>
+                    <div class="ORlaptop">
+                        <div class="ORscreen"></div>
+                        <div class="ORkeyboard"></div>
+                    </div>
+                </div>
             </div>
         </div>
     )
