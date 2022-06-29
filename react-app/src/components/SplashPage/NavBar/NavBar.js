c21a77c67c2d54a50e2409afd4573007a10e7926
Sona Jasani <jasanisona@gmail.com>
2022-06-29 00:14:49 -0400

    splash-page-text

--- react-app/src/components/SplashPage/NavBar/NavBar.js
+++ react-app/src/components/SplashPage/NavBar/NavBar.js
@@ -2,7 +2,6 @@
 import React  from 'react';
 import { useSelector} from 'react-redux';
 import { NavLink } from 'react-router-dom';
-import LogoutButton from '../../auth/LogoutButton';
 
 import './navBar.css'
 import logo from './logo.png'
@@ -15,32 +14,41 @@ const NavBar = () => {
 
   return (
         <div className="Nav-bar">
-          <nav className="Navigation-bar">
-            <div id='splash-logo'>
-              <img id="splash-imgLogo" src={logo} />
-              <NavLink id="splash-homeBtn" to='/' exact={true} activeClassName='active' >
-                WhoDis?
-              </NavLink>
-            </div>
-            <div className='splash-github'>
-              <a id="nav-github"
-                href='https://github.com/Dedition/WhoDis/wiki'
-                target="_blank"
-                rel="noopener noreferrer" >
-                GitHub
-              </a>
-            </div>
-            <div>
-              <NavLink id="nav-about" to='/about' exact={true} activeClassName='active' >
-                About Us
-              </NavLink>
-            </div>
-            <div>
-              <NavLink id="nav-login" to='/login' exact={true} activeClassName='active'>
-                Login
-              </NavLink>
-            </div>
-          </nav>
+          <div className="splash-nav">
+            <nav className="Navigation-bar">
+              <div id='splash-logo'>
+                <img id="splash-imgLogo" src={logo} />
+                <NavLink id="splash-homeBtn" to='/' exact={true} activeClassName='active' >
+                  WhoDis?
+                </NavLink>
+              </div>
+              <div className='splash-github'>
+                <a id="nav-github"
+                  href='https://github.com/Dedition/WhoDis/wiki'
+                  target="_blank"
+                  rel="noopener noreferrer" >
+                  GitHub
+                </a>
+              </div>
+              <div>
+                <NavLink id="nav-about" to='/about' exact={true} activeClassName='active' >
+                  About Us
+                </NavLink>
+              </div>
+              <div>
+                <NavLink id="nav-login" to='/login' exact={true} activeClassName='active'>
+                  Login
+                </NavLink>
+              </div>
+            </nav>
+          </div>
+
+          <div className="splash-text-container">
+              <h1 id="splash-title" >New Chat, Who Dis???</h1>
+              <p id="splash-txt">
+                  ...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.
+              </p>
+          </div>
         </div>
   );
 }
--- react-app/src/components/SplashPage/SplashPage.js
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -2,9 +2,10 @@
 import React  from 'react';
 import { useSelector} from 'react-redux';
 import { NavLink } from 'react-router-dom';
-import NavBar from './NavBar/NavBar';
 
+import NavBar from './NavBar/NavBar';
 import './splashPage.css'
+// import SplashText from './SplashText/SplashText';
 
 /********************************************************************************************************************/
 
@@ -12,16 +13,7 @@ const SplashPage = () => {
     return (
         <div className='splash-main-page'>
             <NavBar />
-            {/* <div className='splash-text'>
-                <div className='splash-slogan'>
-                    <div>
-                        New Chat, Who Dis???
-                    </div>
-                </div>
-                <div className='splash-para'>
-                    <p>...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.</p>
-                </div>
-                </div>
+            {/* 
 
                 <div>
                 <div>
