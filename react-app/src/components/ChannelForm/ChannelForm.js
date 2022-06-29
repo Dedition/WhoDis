045d0a22574dfd9f61d88ef7a377c84d082cd711
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-06-29 12:30:20 -0400

    Fixed issue that made the channel form appear on a new server. But still no way to close the modal (besides the weird mthood of havingit open and close eeverywhere onClick of the  right sidebar

--- react-app/src/components/ChannelForm/ChannelForm.js
+++ react-app/src/components/ChannelForm/ChannelForm.js
@@ -10,6 +10,7 @@ const ChannelForm = () => {
   const [errors, setErrors] = useState([]);
   const [name, setName] = useState("");
 
+
   const history = useHistory();
   const dispatch = useDispatch();
 
@@ -32,22 +33,22 @@ const ChannelForm = () => {
   return (
     <div className='modal'>
       <div className='form'>
-      <form onSubmit={submitForm}>
+        <form onSubmit={submitForm}>
           <div className='create-text'>Create New Channel!</div>
-        <label htmlFor='name'>Name</label>
-        <input
-          name="name"
-          type="text"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-        <button type="submit">
-          Submit
-        </button>
-      </form>
-    </div>
+          <label htmlFor='name'>Name</label>
+          <input
+            name="name"
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+          <button type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
       {/* animation */}
       {/* <div class="left">
         <div class="cat">
@@ -88,7 +89,7 @@ const ChannelForm = () => {
 
 
 
-      
+
     </div>
   )
 
--- react-app/src/components/RightSidebar/RightSidebar.js
+++ react-app/src/components/RightSidebar/RightSidebar.js
@@ -1,20 +1,32 @@
-import './rightsidebar.css'
-import Channels from '../Channels/Channels'
-import LogoutButton from '../auth/LogoutButton'
+import './rightsidebar.css';
+import { useState } from 'react';
+import Channels from '../Channels/Channels';
+import LogoutButton from '../auth/LogoutButton';
+import ChannelForm from '../ChannelForm/ChannelForm';
+
 const RightSidebar = () => {
+    const [form, setForm] = useState(false);
+
+    const showForm = () => setForm(!form);
+
+
+
     return (
-        <div className='right-sidebar'>
+        <div onClick={showForm} className='right-sidebar'>
             <div className='rs-content'>
-            <div className='channel-name'>
-                <p id='channel-title'>Channel Name</p>
-            </div>
-            <div className='create-channel'>
-                <p className='text-chnl'>Text Channel</p>
-                <button id='channel-create-btn'>
-                    <i className="fa-solid fa-plus channel-add"></i>
-                </button>
-            </div>
-            <Channels/>
+                <div className='channel-name'>
+                    <p id='channel-title'>Channel Name</p>
+                </div>
+                <div className='create-channel'>
+                    <p className='text-chnl'>Text Channel</p>
+                    <button onClick={showForm} id='channel-create-btn'>
+                        <i className="fa-solid fa-plus channel-add"></i>
+                    </button>
+                </div>
+                {form &&
+                    <ChannelForm />
+                }
+                <Channels />
             </div>
         </div>
     )
