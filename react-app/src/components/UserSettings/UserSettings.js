9edcfebcbe7a70f614745404c60c1a3c90d6ba54
Sona Jasani <jasanisona@gmail.com>
2022-06-30 14:09:05 -0400

    commit after the merge

--- react-app/src/components/UserSettings/UserSettings.js
+++ react-app/src/components/UserSettings/UserSettings.js
@@ -4,6 +4,8 @@ import { Redirect, useHistory, NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
 import { signUp } from '../../store/session';
 
+import './UserSettings.css'
+
 
 const UserProfile = () => {
   const user = useSelector(state => state.session.user);
@@ -19,29 +21,36 @@ const UserProfile = () => {
 
   return (
     <div className='main-user-div'>
-      <span className="logout-div">
+      <div className="logout-div">
         <LogoutButton />
-      </span>
-      <span>
+      </div>
+      <div className='user-content-div'>
         <h1>My Account</h1>
         {/* <div>
           {user?.profile_pic_url}
         </div> */}
-        <div className='user-name'>
-          {user?.username}
-        </div>
-        <div  className='user=name2'>
-          {user?.username}
-        </div>
-        <div className='user-email'>
-          {user?.email}
+        <div className='user-info-div'>
+          <div className='user-name-initial'>
+            {user?.username.charAt(0)}
+          </div>
+          <div className='user-name'>
+            {user?.username}
+          </div>
+          <div className='user-subinfo'>
+            <div  className='user=name2'>
+              {user?.username}
+            </div>
+            <div className='user-email'>
+              {user?.email}
+            </div>
+          </div>
         </div>
         <div>
           <button onClick={handleCancelClick}>
             Back
           </button>
         </div>
-      </span>
+      </div>
     </div>
   )
 }
